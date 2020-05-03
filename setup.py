0a21a50bb94975f6090d6e2f9aac7f3259af008f
Àngel Àlvarez <angel@nan-tic.com>
2020-05-03 12:17:18 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-# This file is part account_bank_statement_paypal module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# encoding: utf-8
 
 from setuptools import setup
 import re
@@ -12,8 +10,13 @@ from configparser import ConfigParser
 MODULE = 'account_bank_statement_paypal'
 PREFIX = 'trytonspain'
 MODULE2PREFIX = {
+    'account_move_draft': 'trytonspain',
     'account_bank_statement': 'trytonspain',
-    }
+    'account_bank_statement_account': 'trytonspain'}
+OWNER = {
+    'nantic':'NaN-tic',
+    'trytonzz':'nanticzz',
+}
 
 
 def read(fname):
@@ -31,51 +34,69 @@ def get_require_version(name):
         major_version, minor_version + 1)
     return require
 
+def get_requires(depends='depends'):
+  requires = []
+  for dep in info.get(depends, []):
+      if not re.match(r'(ir|res)(\W|$)', dep):
+          prefix = MODULE2PREFIX.get(dep, 'trytond')
+          owner = OWNER.get(prefix, prefix)
+          if prefix == 'trytond':
+              requires.append(get_require_version('%s_%s' % (prefix, dep)))
+          else:
+              requires.append(
+                  '%(prefix)s-%(dep)s@git+https://github.com/%(owner)s/'
+                  'trytond-%(dep)s.git@%(branch)s'
+                  '#egg=%(prefix)s-%(dep)s-%(series)s'%{
+                          'prefix': prefix,
+                          'owner': owner,
+                          'dep':dep,
+                          'branch': branch,
+                          'series': series,})
+
+  return requires
+
 config = ConfigParser()
 config.readfp(open('tryton.cfg'))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
         info[key] = info[key].strip().splitlines()
+
 version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-requires = ['retrofix']
-for dep in info.get('depends', []):
-    if not re.match(r'(ir|res)(\W|$)', dep):
-        prefix = MODULE2PREFIX.get(dep, 'trytond')
-        requires.append(get_require_version('%s_%s' % (prefix, dep)))
-requires.append(get_require_version('trytond'))
+requires = []
 
-tests_require = [get_require_version('proteus')]
 series = '%s.%s' % (major_version, minor_version)
 if minor_version % 2:
-    branch = 'default'
+    branch = 'master'
 else:
     branch = series
-dependency_links = [
-    ('hg+https://bitbucket.org/trytonspain/'
-        'trytond-account_bank_statement@%(branch)s'
-        '#egg=trytonspain-account_bank_statement-%(series)s' % {
-            'branch': branch,
-            'series': series,
-            }),
+
+requires += get_requires('depends')
+
+tests_require = [
+    get_require_version('proteus'),
+
     ]
+tests_require += get_requires('extras_depend')
+requires += []
+
+dependency_links = []
+
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
-    description='Tryton Account Bank Statement ES CSB43 Module',
+    description='',
     long_description=read('README'),
-    author='TrytonSpain',
-    author_email='info@trytonspain.com',
-    url='https://bitbucket.org/trytonspain/',
-    download_url="https://bitbucket.org/trytonspain/trytond-%s" % MODULE,
-    keywords='',
+    author='trytonspain',
+    url='http://www.nan-tic.com/',
+    download_url='https://github.com:trytonspain/trytond-account_bank_statement_paypal',
     package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
         'trytond.modules.%s' % MODULE,
@@ -83,9 +104,12 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
-                'icons/*.svg', 'tests/*.rst']),
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml',
+            'icons/*.svg']),
         },
+    project_urls = {
+       "Source Code": 'https://github.com:trytonspain/trytond-account_bank_statement_paypal'
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -94,26 +118,12 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
         'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
-        'Natural Language :: Czech',
-        'Natural Language :: Dutch',
         'Natural Language :: English',
-        'Natural Language :: French',
-        'Natural Language :: German',
-        'Natural Language :: Hungarian',
-        'Natural Language :: Italian',
-        'Natural Language :: Portuguese (Brazilian)',
-        'Natural Language :: Russian',
-        'Natural Language :: Slovenian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Office/Business',
         ],
     license='GPL-3',
@@ -127,8 +137,5 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-    use_2to3=True,
-    convert_2to3_doctests=[
-        'tests/scenario_account_bank_statement_paypal.rst',
-        ],
+
     )
